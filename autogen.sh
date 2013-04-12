d96beacad525d27fa7b0705563c6773fd19b8853
Piotr Esden-Tempski <piotr@esden.net>
2013-04-12 11:33:34 -0700

    Simplified autogen.sh to use set -e instead of the clunky && chaining.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,9 @@
 #
 # WARNING: if you run this you will change the versions
 # of the tools which are used and, maybe, required!
+
+set -e
+
 touch Makefile.am configure.ac
 
 OS=`uname`
@@ -14,26 +17,21 @@ else
     LIBTOOLIZE=libtoolize
 fi
 
-{
-    echo "running libtoolize" >&2
-    $LIBTOOLIZE --copy --automake
-} && {
-    echo "running aclocal" >&2
-    if [ -n "$CHECK_DIR" ]; then
-	aclocal -I m4 -I $CHECK_DIR
-    else
-	aclocal -I m4
-    fi
-} && {
-    echo "running autoheader [ignore the warnings]" >&2
-    autoheader
-} && {
-    echo "running automake" >&2
-    automake --foreign -a -c --add-missing
-} && {
-    echo "running autoconf" >&2
-    autoconf
-} && echo "autogen complete" >&2 || {
-    echo "ERROR: autogen.sh failed, autogen is incomplete" >&2
-    exit 1
-}
+echo "running libtoolize" >&2
+$LIBTOOLIZE --copy --automake
+    
+echo "running aclocal" >&2
+if [ -n "$CHECK_DIR" ]; then
+    aclocal -I m4 -I $CHECK_DIR
+else
+    aclocal -I m4
+fi
+    
+echo "running autoheader [ignore the warnings]" >&2
+autoheader
+
+echo "running automake" >&2
+automake --foreign -a -c --add-missing
+
+echo "running autoconf" >&2
+autoconf
